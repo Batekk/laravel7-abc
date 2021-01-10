64b99fdd231ccd51f594486f258df1d23899c863
host <batekkmail@gmail.com>
2021-01-11 00:38:31 +0600

    Вывод отношений компании в представление

--- resources/views/company/form.blade.php
+++ resources/views/company/form.blade.php
@@ -28,7 +28,35 @@
                             <label>user_ids</label>
                             <p class='card-text'>
                                 @foreach($company->users as $user)
-                                    <a href="{{ route('users.edit', $user->_id) }}">{{ $user->name}}</a>@if($loop->count > 1 && !$loop->last),@endif
+                                    <a href="{{ route('users.edit', $user->_id) }}">{{ $user->name}}</a>@if($loop->count > 1 && !$loop->last)
+                                        ,@endif
+                                @endforeach
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-3">
+                        <div class="form-group-lg">
+                            <label>category</label>
+                            <p class='card-text'>
+                                @foreach($company->category as $category)
+                                    <a href="{{ route('category.edit', $category->_id) }}">{{ $category->name}}</a>@if($loop->count > 1 && !$loop->last)
+                                        ,@endif
+                                @endforeach
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-3">
+                        <div class="form-group-lg">
+                            <label>services</label>
+                            <p class='card-text'>
+                                @foreach($company->category as $category)
+                                    @foreach($category->services as $service)
+                                        <a href="{{ route('services.edit', $service->_id) }}">{{ $service->name}}</a>@if($loop->count > 1 && !$loop->last)
+                                            ,@endif
+                                    @endforeach
                                 @endforeach
                             </p>
                         </div>
