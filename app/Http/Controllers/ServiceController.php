39094ea4097622e0b5faa263e0ec05fda6d07c66
host <batekkmail@gmail.com>
2021-01-11 00:30:23 +0600

    5#issue

---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\mongo\Service;
+use Exception;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use Illuminate\View\View;
+
+class ServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Service $services
+     * @return Factory|JsonResponse|View
+     */
+    public function index(Service $services)
+    {
+        if (request()->ajax()) {
+            return $services::dataTable()->toJson();
+        }
+
+        return view('layouts.dataTable.index', [
+            'html' => $services->html(),
+            'breadcrumb' => 'Сервисы',
+            'url_create' => route('services.create'),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @param Service $service
+     * @return Factory|View
+     */
+    public function create(Service $service)
+    {
+        return view('services.form', compact('service'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return RedirectResponse|Redirector
+     */
+    public function store(Request $request)
+    {
+        Service::createFromRequest($request);
+        return redirect(route('services.index'))->with('success', 'Успех');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Service $service
+     * @return Factory|View
+     */
+    public function edit(Service $service)
+    {
+        return view('services.form', compact('service'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Service $service
+     * @return RedirectResponse|Redirector
+     */
+    public function update(Request $request, Service $service)
+    {
+        $service->update($request->input());
+        return redirect(route('services.index'))->with('success', 'Обновлено');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Service $service
+     * @return RedirectResponse
+     * @throws Exception
+     */
+    public function destroy(Service $service)
+    {
+        $service->delete();
+        return redirect()->route('services.index')
+            ->with('alert', 'Удален');
+    }
+}
--- app/Models/mongo/Service.php
+++ app/Models/mongo/Service.php
@@ -1,10 +1,44 @@
 <?php
 
-namespace App;
+namespace App\Models\mongo;
 
-use App\Models\mongo\MongoModel;
+use App\Models\DataTableTrait;
+use Jenssegers\Mongodb\Eloquent\SoftDeletes;
+use Html;
 
 class Service extends MongoModel
 {
-    protected $fillable = ['category_id', 'name'];
+    use SoftDeletes, DataTableTrait;
+
+    protected $fillable = ['name', 'category_id'];
+    protected $columns = ['_id', 'name', 'category_id', 'Action'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    static function dataTable()
+    {
+        return datatables(self::query()
+            ->whereNull('deleted_at'))
+            ->editColumn('category_id', function (self $data) {
+                return [
+                    Html::link(route('category.edit', $data->category->_id), $data->category->name, [
+                        'class' => 'btn btn-xs btn-link'])
+                ];
+            })
+            ->editColumn('Action', function (self $data) {
+                return [
+                    Html::link(route('services.edit', $data->_id), 'Edit', [
+                        'class' => 'btn btn-xs btn-info'
+                    ]),
+                    Html::link('#', 'Del', [
+                        'class' => 'btn btn-xs btn-danger',
+                        'onClick' => sprintf('deleteData("%s")',
+                            route('services.destroy', $data->_id))
+                    ])
+                ];
+            });
+    }
 }
